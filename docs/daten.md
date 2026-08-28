69b8c8cec1a250915c48bdef23ac3e839f4af198
Thomas Eses <thomas.eses@lead-dealer.com>
2026-08-28 13:11:55 +0200

    update

---
+++ docs/daten.md
@@ -0,0 +1,20 @@
+
+# Pricefile
+- eine Datei
+Dateiaert: txt
+Format: 
+
+
+# Others
+- eine Datei
+
+# D-Lists
+- mehrere Dateien
+
+
+# Content Lists
+- mehrere Dateien
+- 2 Sheets je Datei
+
+# Applikations Lists
+- mehrere Dateien
---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+
+
+# get config
+config = ""
+
+# get db connection
+db_conn = ""
+
+# logger initialisieren
+logger = ""
+
+# Pipeline instanzieieren
+#p = ETlPricefile(db_conn, logger, config)
+#p = ETlGPRS(db_conn, logger, config)
+#p.run()
+
+
+pipelines = [
+    ETlPricefile(db_conn, logger, config),
+    ETLGPRS(db_conn, logger, config)
+]
+for pipeline in pipelines:
+    try:
+        pipeline.run()
+    except Exception as e:
+        print(f"Fehler in {pipeline.__class__.__name__}: {e}")
